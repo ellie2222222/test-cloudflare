d893ab8113b17aff97d0c4696a50dbb765ae963c
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-10-21 19:39:25 +0700

    chore: remove static file serving

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/live-stream', express.static(path.join(__dirname, 'live-stream')));
+
 // Log API requests
 app.use((req, res, next) => {
   console.log(req.method + " " + req.path);
