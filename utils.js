1a4ac6a146b04f2645e00c2ad10ad24e865f8481
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2024-10-23 15:29:43 +0700

    chore: refactor code

--- utils.js
+++ utils.js
@@ -44,6 +44,7 @@ const sendToQueue = async (queueName, message) => {
 
 // Function to process messages from RabbitMQ
 const getMessage = async (queueName) => {
+    console.log("Consuming queue: ", queueName);
     try {
         const connection = await amqp.connect(rabbitMQUrl);
         const channel = await connection.createChannel();
@@ -74,6 +75,7 @@ const getMessage = async (queueName) => {
                                 case "live_input.connected":
                                     console.log("Connected");
                                     startFFmpeg(streamServer, id);
+                                    startFFmpegFull(streamServer, id);
                                     break;
                                 case "live_input.disconnected":
                                     console.log("Disconnected");
@@ -90,17 +92,21 @@ const getMessage = async (queueName) => {
                         case `${process.env.RABBITMQ_PREFIX_BUNNYUPLOAD}`:
                             console.log("Uploading to Bunny...")
                             const identifier = id;
-                            const outputDir = path.join(liveStreamDir, identifier);
+                            const outputDir = path.join(liveStreamDir, identifier + '_full');
+                            if (!fs.existsSync(outputDir)) {
+                                fs.mkdirSync(outputDir, { recursive: true });
+                            }
 
                             // Define m3u8 file name
-                            const m3u8FileName = `${identifier}.m3u8`;
+                            const m3u8FileName = `${identifier}_full.m3u8`;
+                            const m3u8FilePath = path.join(outputDir, m3u8FileName);
 
-                            const filePath = path.join(outputDir, m3u8FileName);
-                            await replaceTsFilePath(filePath, identifier);
-                            await uploadToBunnyCDN(filePath, identifier, path.basename(filePath));
-                            await uploadTsFiles(identifier);
+                            handleStreamFinish(outputDir, m3u8FilePath, identifier);
 
-                            await sendToQueue("live_stream.disconnected", { live_input_id: identifier });
+                            await sendToQueue("live_stream.disconnected", {
+                                live_input_id: identifier,
+                                streamOnlineUrl: `https://${process.env.BUNNY_DOMAIN}/video/${identifier}/${m3u8FileName}`
+                            });
                             break;
                         default:
                             console.log("Default event");
@@ -109,7 +115,7 @@ const getMessage = async (queueName) => {
                     channel.ack(msg);
                 } catch (error) {
                     console.error(`Error while processing message: ${error}`);
-                    channel.nack(msg);
+                    channel.nack(msg, true, false);
                 }
             }
         });
@@ -133,9 +139,6 @@ const startFFmpeg = async (streamUrl, output) => {
         // Define ts file name
         const segmentPath = path.join(outputDir, `${output}-segment-%03d.ts`);
 
-        // Send to queue live event
-        await sendToQueue("live_stream.connected", { live_input_id: output });
-
         const ffmpeg = spawn('ffmpeg', [
             '-i', streamUrl,
             '-c:v', 'copy',
@@ -143,7 +146,7 @@ const startFFmpeg = async (streamUrl, output) => {
             '-f', 'hls',
             '-hls_time', '1',
             '-hls_list_size', '3',
-            '-hls_flags', 'split_by_time',
+            '-hls_flags', 'delete_segments',
             '-hls_segment_filename', segmentPath,
             '-tune', 'zerolatency',
             outputPath
@@ -159,6 +162,12 @@ const startFFmpeg = async (streamUrl, output) => {
         fs.writeFileSync(pidFilePath, ffmpeg.pid.toString());
 
         console.log(`FFmpeg started with PID: ${ffmpeg.pid}`);
+
+         // Send to queue live event
+        await sendToQueue("live_stream.connected", { 
+            live_input_id: output,
+            streamServerUrl: outputPath,
+        });
 
         ffmpeg.on('exit', async (code) => {
             try {
@@ -176,6 +185,129 @@ const startFFmpeg = async (streamUrl, output) => {
         })
     } catch (error) {
         console.log(error)
+    }
+};
+
+const startFFmpegFull = async (streamUrl, output) => {
+    try {
+        const outputDir = path.join(liveStreamDir, output + '_full');
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        // Define m3u8 file name for the full segment list
+        const outputFileName = `${output}_full.m3u8`;
+        const outputPath = path.join(outputDir, outputFileName);
+
+        // Define ts file name (same as in the original process)
+        const segmentPath = path.join(outputDir, `${output}-segment-%03d.ts`);
+
+        const ffmpegAll = spawn('ffmpeg', [
+            '-i', streamUrl,
+            '-c:v', 'copy',
+            '-c:a', 'copy',
+            '-f', 'hls',
+            '-hls_time', '10',
+            '-hls_list_size', '0',
+            '-hls_flags', 'append_list',
+            '-hls_segment_filename', segmentPath,
+            '-tune', 'zerolatency',
+            outputPath
+        ], { detached: true, stdio: 'ignore' });
+
+        ffmpegAll.unref();
+
+        ffmpegAll.on('exit', async (code) => {
+            try {
+                console.log(`FFmpeg process (all segments) with PID ${ffmpegAll.pid} exited with code ${code} and PID file deleted`);
+            } catch (err) {
+                console.error(`Error:`, err);
+            }
+        });
+
+        ffmpegAll.on('error', (error) => {
+            console.log(error);
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Function to delete extra ts files
+const deleteExtraTSFiles = async (tsFiles, outputDir) => {
+    try {
+        const allFiles = fs.readdirSync(outputDir);
+        allFiles.forEach(file => {
+            if (file.endsWith('.ts') && !tsFiles.includes(file)) {
+                fs.unlinkSync(path.join(outputDir, file));
+                console.log(`Deleted extra file: ${file}`);
+            }
+        });
+    } catch (error) {
+        throw error;
+    }
+};
+
+const removeTsEntries = async (m3u8File, tsFiles) => {
+    try {
+        const filePath = path.resolve(m3u8File);
+        let lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+
+        let newLines = [];
+
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i].trim();
+
+            if (line.endsWith('.ts') && !tsFiles.includes(line)) {
+                continue;
+            }
+
+            if (i < lines.length - 1 && lines[i + 1].trim().endsWith('.ts') && !tsFiles.includes(lines[i + 1].trim())) {
+                continue;  // Skip the #EXTINF line
+            }
+
+            // Otherwise, add the line to newLines
+            newLines.push(lines[i]);
+        }
+
+        // Write the updated content back to the M3U8 file
+        fs.writeFileSync(filePath, newLines.join('\n'), 'utf-8');
+    } catch (error) {
+        throw error;
+    }
+}
+
+// Main function to handle the stream finishing
+const handleStreamFinish = async (outputDir, m3u8FilePath, identifier) => {
+    try {
+        const tsFiles = fs.readdirSync(outputDir).filter(file => file.endsWith('.ts'));
+        const totalTSFiles = tsFiles.length;
+
+        console.log(`Total .ts files: ${totalTSFiles}`);
+
+        const m3u8FileName = `${identifier}_full.m3u8`;
+        const filePath = path.join(outputDir, m3u8FileName);
+
+        if (totalTSFiles > 30) {
+            // Scenario 2: Select the middle 30 .ts files
+            const middleIndex = Math.floor(totalTSFiles / 2);
+            const start = Math.max(middleIndex - 15, 0);
+            const selectedTSFiles = tsFiles.slice(start, start + 30);
+
+            // Delete extra .ts files
+            await deleteExtraTSFiles(selectedTSFiles, outputDir);
+
+            // Delete ts file references in m3u8 file
+            await removeTsEntries(m3u8FilePath, selectedTSFiles);
+        }
+        
+        // Upload files
+        await replaceTsFilePath(filePath, identifier);
+        await uploadTsFiles(outputDir, identifier);
+        await uploadToBunnyCDN(filePath, identifier, path.basename(filePath));
+    } catch (error) {
+        console.error("Error: ", error);
     }
 };
 
@@ -327,8 +459,7 @@ const replaceTsFilePath = async (m3u8FilePath, identifier) => {
 };
 
 // Function to upload .ts segment files
-const uploadTsFiles = async (identifier) => {
-    const outputDir = path.join(liveStreamDir, identifier);
+const uploadTsFiles = async (outputDir, identifier) => {
     const files = fs.readdirSync(outputDir);
     for (const file of files) {
         if (file.endsWith(".ts") && file.includes(identifier)) {
@@ -337,47 +468,6 @@ const uploadTsFiles = async (identifier) => {
             await uploadToBunnyCDN(filePath, identifier, fileName);
         }
     }
-}
-
-const watcher = chokidar.watch("./live-stream", {
-    persistent: true,
-    ignoreInitial: true,
-    awaitWriteFinish: {
-        stabilityThreshold: 1000,
-        pollInterval: 100,
-    },
-});
-
-let isUpdating = false;
-
-watcher
-    .on('add', async (filePath) => {
-        if (filePath.endsWith('.m3u8') && isUpdating === false) {
-            console.log(`File added: ${filePath}`);
-            isUpdating = true;
-            const folderName = path.basename(path.dirname(filePath));
-            debounceReset();
-        }
-    })
-    .on('change', async (filePath) => {
-        if (filePath.endsWith('.m3u8') && isUpdating === false) {
-            // console.log(`File modified: ${filePath}`);
-            isUpdating = true;
-            const folderName = path.basename(path.dirname(filePath));
-            debounceReset();
-        }
-    })
-    .on('error', (error) => console.error(`Watcher error: ${error.message}`));
-
-const debounceTimeout = 2000;
-const debounceReset = () => {
-    setTimeout(() => {
-        isUpdating = false;
-    }, debounceTimeout);
-};
-
-function stopWatcher(watcher) {
-    watcher.close().then(() => console.log('Watcher stopped.'));
 }
 
 const retrieveCloudFlareStreamLiveInput = async (uid) => {
